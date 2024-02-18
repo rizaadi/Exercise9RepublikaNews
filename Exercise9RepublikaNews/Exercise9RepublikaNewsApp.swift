a9c2a99d7f15c7a26f6f0f5e8a24a7c0551b429b
rizaadi <rizaadi890@gmail.com>
2024-02-18 12:11:59 +0700

    Initial Commit

---
+++ Exercise9RepublikaNews/Exercise9RepublikaNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Exercise9RepublikaNewsApp.swift
+//  Exercise9RepublikaNews
+//
+//  Created by Riza Adi Kurniawan on 18/02/24.
+//
+
+import SwiftUI
+
+@main
+struct Exercise9RepublikaNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
